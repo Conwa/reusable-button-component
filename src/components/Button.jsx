add37eb57168abbb7e448e5697b2f16485dd64e4
Conwa <conradodelcarlo97@gmail.com>
2023-06-22 22:21:38 -0300

    almost done

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,13 +1,14 @@
 const ButtonVariant = {
   text: "bg-white hover:bg-blue-100 text-blue-500 border-0",
   outline: "border-blue-500 bg-white hover:bg-blue-100 text-blue-500",
-  default: "hover:border-gray-500 bg-gray-200 hover:bg-gray-300 text-slate-600",
+  default:
+    "hover:border-gray-400 border-gray-300 bg-gray-300 hover:bg-gray-400 text-slate-600",
 };
 
 const ButtonVariantHover = {
   text: "bg-blue-100 text-blue-500 border-0",
   outline: "border-blue-500 bg-blue-100 text-blue-500",
-  default: "border-gray-500 bg-gray-300 text-slate-600",
+  default: "border-gray-400 bg-gray-400 text-slate-600",
 };
 
 const ButtonSize = {
@@ -19,16 +20,21 @@ const ButtonSize = {
 const ButtonDisabled =
   "disabled:pointer-events-none disabled:text-slate-400 disabled:hover:bg-inherit";
 
+const ButtonDisableShadow =
+  "bg-blue-600 text-white border-blue-600 hover:bg-blue-600 hover:border-blue-600";
+
 const Button = (props) => {
   const {
     variant = "default",
     disabled = false,
+    disableShadow = false,
     startIcon,
     endIcon,
     size = "medium",
   } = { ...props };
 
   const labels = { ...props };
+  console.log(props.variant);
 
   const buttonStyle = `w-fit rounded-lg border-2 font-semibold `;
 
@@ -37,15 +43,45 @@ const Button = (props) => {
   ${ButtonVariant[variant]} 
   ${ButtonSize[size]} 
   ${disabled ? ButtonDisabled : null}
+  ${disableShadow ? ButtonDisableShadow : null}
   `;
   const buttonStyleDefinitiveHover = `${buttonStyle} 
   ${ButtonVariantHover[variant]} 
   ${ButtonSize[size]} 
-  ${disabled ? ButtonDisabled : null}`;
+  ${disabled ? ButtonDisabled : null} 
+  `;
+  if (props.variant && labels.disabled !== true) {
+    return (
+      <div className="flex w-6/12 justify-between gap-16">
+        {" "}
+        <div className="flex  flex-col">
+          <p className="pb-3 text-sm text-slate-500 ">
+            {" "}
+            {`<Button`}
+            {Object.entries(labels).map((el) => (
+              // eslint-disable-next-line react/jsx-key
+              <span>
+                {" "}
+                {el[0]}= "{el[1].toString()}"
+              </span>
+            ))}
+            {`/>`}{" "}
+          </p>
 
-  return (
-    <div className="flex gap-16">
-      {" "}
+          <button disabled={disabled} className={buttonStyleDefinitive}>
+            {startIcon ? <img src={startIcon} /> : null} Default {endIcon}
+          </button>
+        </div>
+        <div className="flex flex-col">
+          <p className="pb-3 text-sm text-slate-400">&:hover, &:focus</p>
+          <button disabled={disabled} className={buttonStyleDefinitiveHover}>
+            Default
+          </button>
+        </div>
+      </div>
+    );
+  } else {
+    return (
       <div className="flex flex-col">
         <p className="pb-3 text-sm text-slate-500 ">
           {" "}
@@ -58,19 +94,25 @@ const Button = (props) => {
           ))}
           {`/>`}{" "}
         </p>
-
-        <button disabled={disabled} className={buttonStyleDefinitive}>
-          {startIcon} Default {endIcon}
-        </button>
-      </div>
-      <div className="flex flex-col">
-        <p className="pb-3 text-sm text-slate-400">&:hover, &:focus</p>
-        <button disabled={disabled} className={buttonStyleDefinitiveHover}>
+        <button
+          disabled={disabled}
+          className={buttonStyleDefinitive}
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            gap: ".3rem",
+          }}
+        >
+          {startIcon ? (
+            <img src={startIcon} className="m-0 h-7 w-auto" />
+          ) : null}
           Default
+          {endIcon ? <img src={endIcon} className="m-0 h-7 w-auto" /> : null}
         </button>
       </div>
-    </div>
-  );
+    );
+  }
 };
 
 export default Button;
--- src/components/ButtonList.jsx
+++ src/components/ButtonList.jsx
@@ -1,16 +1,33 @@
+import Logo from "../assets/heart.svg";
 import Button from "./Button";
 
 const ButtonList = () => {
   return (
-    <div className="flex w-full flex-col items-start gap-4 ps-12 pt-8">
+    <div className="flex w-full flex-col items-start gap-4 pb-6 ps-12 pt-8">
       <h1
         style={{ fontFamily: "Poppins" }}
         className="pb-4 text-xl font-semibold text-slate-500"
       >
         Buttons
       </h1>
-      <Button variant={"default"} />
-      <Button disabled={true} />
+
+      <Button />
+      <Button variant={"outline"} />
+      <Button variant={"text"} />
+      <Button disableShadow />
+      <div className="flex w-8/12 justify-between gap-16">
+        <Button disabled />
+        <Button variant={"text"} disabled />
+      </div>
+      <div className="flex w-8/12 justify-between gap-16">
+        <Button startIcon={Logo} />
+        <Button endIcon={Logo} />
+      </div>
+      <div className="flex w-10/12 items-center gap-16">
+        <Button size="small" />
+        <Button size="medium" />
+        <Button size="large" />
+      </div>
     </div>
   );
 };
